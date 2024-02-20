81222123e5d878ca5ce27825e50e6b9006b22ebd
fanislala <137422059+fanislala@users.noreply.github.com>
2024-02-21 03:24:08 +0530

    9
    9

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using CodeFirstNewDatabaseSample;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstNewDatabaseSample
+{
+	class Program
+	{
+		static void Main (String[] args)
+		{
+		}
+	}
+
+	public class Blog
+	{
+		public int BlogId { get; set; }
+		public string Name { get; set; }
+
+		public virtual List<Post> Posts { get; set; }
+	}
+}
+	public class Post
+	{
+    public int PostId { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+
+    public int BlogId { get; set; }
+    public virtual Blog Blog { get; set; }
+	}
+
